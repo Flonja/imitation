72fd43854bff0c8a6bb388c922aff06b8ef0ee60
Flonja <20887403+Flonja@users.noreply.github.com>
2023-12-08 01:00:42 +0100

    Added a give command

--- src/main/java/tech/flonja/imitation/Imitation.java
+++ src/main/java/tech/flonja/imitation/Imitation.java
@@ -2,7 +2,11 @@ package tech.flonja.imitation;
 
 import lombok.Getter;
 import org.bukkit.NamespacedKey;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import tech.flonja.imitation.items.CustomItem;
 import tech.flonja.imitation.items.CustomItemRegistry;
 
 @Getter
@@ -13,5 +17,17 @@ public final class Imitation extends JavaPlugin {
     @Override
     public void onEnable() {
         this.getServer().getPluginManager().registerEvents(new EventListener(this), this);
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if(!(sender instanceof Player player)) return false;
+        if(!command.getName().equalsIgnoreCase("item")) return false;
+        if(args.length != 1) return false;
+
+        CustomItem customItem = this.registry.getCustomItem(args[0]);
+        if(customItem == null) return false;
+        player.getInventory().addItem(this.registry.getItemStack(customItem));
+        return true;
     }
 }
